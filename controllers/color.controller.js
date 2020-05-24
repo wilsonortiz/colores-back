16705ecfbdbacfb0c6a8f6ce19687ca163f10e14
Wilson Ortiz Troncoso <wilsonortiztr@gmail.com>
2020-05-24 14:50:34 -0400

    paginación

--- controllers/color.controller.js
+++ controllers/color.controller.js
@@ -1,10 +1,27 @@
 const Color = require("../models/color.model");
 
 const getColores = (req, res) => {
-  Color.findAll().then((colores) => {
-    console.log(colores);
-    res.status(200).send({ colores: colores });
-  });
+
+  let page = req.query.page;
+  let limit = req.query.limit;
+
+  let offset = 0 + (page - 1) * limit
+
+  Color.findAndCountAll({
+    limit,
+    offset: offset
+  }).then((result) => {
+    let total_paginas = 1
+      if(result.count / limit > 1){
+        total_paginas = Math.ceil(result.count / limit);
+      }
+
+      res.status(200).send({ total_elementos: result.count, pagina_actual : page, total_paginas: total_paginas,colores: result.rows });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send({ error: true, mensaje: "Error en el servidor" });
+    });
 };
 
 const getColor = (req, res) => {
@@ -12,10 +29,27 @@ const getColor = (req, res) => {
     where: {
       id: req.params.id,
     },
-  }).then((color) => {
-    console.log(color);
-    res.status(200).send({ color: color });
-  });
+  })
+    .then((color) => {
+      if (color !== null) {
+        res.status(200).send({ error: false, color: color });
+      } else {
+        res.status(404)
+          .send({
+            error: true,
+            mensaje: `No existe el color: ${req.params.id}`,
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res
+        .status(500)
+        .send({
+          error: true,
+          mensaje: `Error en el servidor`,
+        });
+    });
 };
 
 const saveColor = (req, res) => {
@@ -27,6 +61,14 @@ const saveColor = (req, res) => {
   }).then((color) => {
     console.log(color);
     res.status(200).send({ color: color });
+  })
+  .catch(error=>{
+    console.error(error);
+    res.status(500)
+    .send({
+      error: true,
+      mensaje: `Error en el servidor`,
+    });
   });
 };
 
@@ -35,18 +77,25 @@ const updateColor = (req, res) => {
     where: {
       id: req.params.id,
     },
-  }).then((color) => {
-    color
-      .update({
-        name: req.body.name,
-        color: req.body.color,
-        pantone: req.body.pantone,
-        year: req.body.year,
-      })
-      .then((color) => {
-        console.log("color actualizado");
-        res.status(200).send({ color: color });
-      });
+  }).then(color => {
+    if(color !== null){
+      color.update({
+          name: req.body.name,
+          color: req.body.color,
+          pantone: req.body.pantone,
+          year: req.body.year,
+        })
+        .then((color) => {
+          console.log("color actualizado");
+          res.status(200).send({ color: color });
+        });
+    }else {
+      res.status(404)
+        .send({
+          error: true,
+          mensaje: `No existe el color: ${req.params.id}`,
+        });
+    }
   });
 };
 
@@ -57,10 +106,10 @@ const deleteColor = (req, res) => {
     },
   }).then((color) => {
     console.log(color);
-    if(color === 1){
-      res.status(200).send({ eliminado: true }); 
-    }else{
-      res.status(500).send({ eliminado: false }); 
+    if (color === 1) {
+      res.status(200).send({ eliminado: true });
+    } else {
+      res.status(500).send({ eliminado: false });
     }
   });
 };
